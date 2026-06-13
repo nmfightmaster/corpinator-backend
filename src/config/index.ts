04cac4d3fe6b09af1f82c628430de92d4c3aabe5
Nathan Fightmaster <nmfightmaster@gmail.com>
2026-06-13 10:43:28 -0700

    fix: scaffold db

--- src/config/index.ts
+++ src/config/index.ts
@@ -11,20 +11,19 @@ interface Config {
     api: {
         prefix: string
     };
+    databaseUrl: string;
 }
 
 const config: Config = {
     port: Number(process.env.PORT) || 3000,
-    
     nodeEnv: process.env.NODE_ENV || 'development',
-    
     logs: {
         level: process.env.LOG_LEVEL || 'warn'
     },
-
     api: {
         prefix: '/api',
     },
+    databaseUrl: process.env.DATABASE_URL || '',
 };
 
 export default config;
