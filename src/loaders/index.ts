9d157ef1ac5690c8a9f25eb8514df5ed0857ae26
Nathan Fightmaster <nmfightmaster@gmail.com>
2026-06-13 11:05:02 -0700

    chore: prisma cleanup

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -4,9 +4,10 @@ import logger from './logger.js';
 
 export default async ({ expressApp }: { expressApp: express.Application }) => {
     logger.info('Loading Express.');
-
     await expressLoader({ app: expressApp });
     logger.info('Express loaded.');
+
+    
 }
 
 export { default as logger } from './logger.js';
