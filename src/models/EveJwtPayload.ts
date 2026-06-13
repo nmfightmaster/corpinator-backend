151770d83c15e578eb48d78392ed48b7389e5549
Nathan Fightmaster <nmfightmaster@gmail.com>
2026-06-13 13:05:26 -0700

    fix: create evejwtpayload model

---
+++ src/models/EveJwtPayload.ts
@@ -0,0 +1,10 @@
+type EveJwtPayload = {
+    name: string;
+    sub: string;
+    scp: string | string[];
+    iss: string;
+    aud: string | string[];
+    exp: number;
+}
+
+export default EveJwtPayload;
